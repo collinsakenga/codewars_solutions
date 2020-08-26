9f9eb9c0c8ab95beff4b84a505212a1224164ba9
Ma Wing Kin <mwk0048@gmail.com>
2020-08-26 18:39:47 +0800

    optimize a solution

--- 6 kyu/6 kyu_Bit Counting.py
+++ 6 kyu/6 kyu_Bit Counting.py
@@ -1,7 +1,2 @@
 def countBits(n):
-    s = str("{0:b}".format(n))
-    count = 0
-    for i in range(len(s)):
-        if s[i] == '1':
-            count += 1
-    return count
+    return bin(n).count("1")
